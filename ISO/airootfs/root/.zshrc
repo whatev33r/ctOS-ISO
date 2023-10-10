4e2c4c25b120721053993a5928bd2b8abd4cd26a
whatev33r <etienne.jax@protonmail.com>
2023-10-10 16:24:28 +0200

    init push

---
+++ ISO/airootfs/root/.zshrc
@@ -0,0 +1,22 @@
+# Prompt
+autoload -U colors && colors
+PROMPT="%{$fg[red]%}[%*]%{$reset_color%} %n@%m:%{$fg[blue]%}%~%{$reset_color%} %{$fg[green]%}$%{$reset_color%} "
+
+# Autocomplete
+autoload -Uz compinit
+compinit
+
+# Color Alias
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias ip="ip --color=auto"
+alias cat="bat -Pp"
+alias tree="tree -C"
+
+# Custom Alias
+alias ll='ls -l'
+alias lsa="ls -lsa"
+
+# Source
+# [[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
+# source .config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
---
+++ ISO/profiledef.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="ctOS"
+iso_label="CTOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Whatev33r <https://github.com/whatev33r/ctOS-ISO>"
+iso_application="ctOS Live/Installer"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
+
